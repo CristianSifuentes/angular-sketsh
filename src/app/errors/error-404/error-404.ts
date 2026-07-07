1ad27427bea5462f2c6302de77531432c3d926ff
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2026-07-07 15:15:45 -0600

    Working routerLinkActive

--- src/app/errors/error-404/error-404.ts
+++ src/app/errors/error-404/error-404.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-error-404',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './error-404.html',
   styleUrl: './error-404.scss',
 })
